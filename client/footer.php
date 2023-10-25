37dc6ce2e9c9d4189f6c41aa221b76c5d4d79e30
rudradcruze <francisrudra@gmail.com>
2023-10-26 00:21:39 +0600

    shop page desing half

--- client/footer.php
+++ client/footer.php
@@ -1,4 +1,24 @@
-    <script src="../assets/js/vendor/jquery-3.4.1.min.js"></script>
+<!--footer area start-->
+<footer class="footer_widgets">
+    <div class="footer_bottom border border-top-1">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="copyright_area">
+                        <p class="copyright-text">&copy; 2023 <a href="#" class="pe-none" tabindex="-1">Maiesha Haque Lira</a>.  <i class="fa fa-heart text-danger"></i> . All Rigts Reserved</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="footer_payment">
+                        <a href="#"><img src="assets/img/icon/payment.png" alt="❤️"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="../assets/js/vendor/jquery-3.4.1.min.js"></script>
     <script src="../assets/js/popper.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
     <script src="../assets/js/owl.carousel.min.js"></script>
